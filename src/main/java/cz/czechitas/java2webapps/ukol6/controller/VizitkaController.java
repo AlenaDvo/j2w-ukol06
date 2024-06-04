e99519fe08321f3ffb8db27b491bce6842473de9
Alena Dvorakova <alenadvorakova@technicky-preklad.cz>
2024-06-04 16:57:25 +0200

    Edit formula.ftlh, vizitka.ftlh, VizitkaController.java.

--- src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
@@ -2,14 +2,14 @@ package cz.czechitas.java2webapps.ukol6.controller;
 
 import cz.czechitas.java2webapps.ukol6.entity.Vizitka;
 import cz.czechitas.java2webapps.ukol6.repository.VizitkaRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
@@ -34,6 +34,21 @@ public class VizitkaController {
                 .addObject("seznam", repository.findAll());
     }
 
+    @GetMapping("/nova")
+    public ModelAndView nova() {
+        return new ModelAndView("formular")
+                .addObject("formular", new Vizitka());
+    }
+
+    @PostMapping("/nova")
+    public String pridat(@ModelAttribute("vizitka") @Valid Vizitka vizitka, BindingResult bindingResult) {
+        vizitka.setId(null);
+        if (bindingResult.hasErrors()) {
+            return "formular";
+        }
+        repository.save(vizitka);
+        return "redirect:/";
+    }
     @GetMapping("/{id:[0-9]+}")
     public Object detail(@PathVariable Integer id) {
         Optional<Vizitka> vizitka = repository.findById(id);
